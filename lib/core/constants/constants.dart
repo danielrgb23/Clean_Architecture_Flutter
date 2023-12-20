395f9c3698b4b5c8f3cc148715303a5b63260db3
Daniel Augusto <danielrgb2@gmail.com>
2023-12-20 08:03:57 -0300

    feat: create useCases

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,1 +1,4 @@
 const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIkey = 'a860012455854f33875dd04e340929c3';
+const String countryQuery = 'br';
+const String categoryQuery = 'general';
--- lib/feature/daily_news/data/repository/article_repository_impl.dart
+++ lib/feature/daily_news/data/repository/article_repository_impl.dart
@@ -1,12 +1,37 @@
+import 'dart:io';
+
+import 'package:clean_arctheture/core/constants/constants.dart';
 import 'package:clean_arctheture/core/resources/data_state.dart';
+import 'package:clean_arctheture/feature/daily_news/data/data_source/remote/news_api_service.dart';
 import 'package:clean_arctheture/feature/daily_news/data/models/article.dart';
 import 'package:clean_arctheture/feature/daily_news/domain/repository/article_repository.dart';
+import 'package:dio/dio.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
-  @override
-  Future<DataState<List<ArticleModel>>> getNewsArticles() {
-    // TODO: implement getNewsArticles
-    throw UnimplementedError();
-  }
+  final NewsApiService _newsApiService;
+  ArticleRepositoryImpl(this._newsApiService);
 
+  @override
+  Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apikey: newsAPIkey,
+        country: countryQuery,
+        category: categoryQuery,
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSucess(httpResponse.data);
+      } else {
+        return DataFailed(DioError(
+          error: httpResponse.response.statusMessage,
+          response: httpResponse.response,
+          type: DioErrorType.response,
+          requestOptions: httpResponse.response.requestOptions,
+        ));
+      }
+    } on DioError catch (e) {
+      return DataFailed(e);
+    }
+  }
 }
